077cbc8919588bc8adf367d92c0ca054468eb6b1
Ken Moore <kenmoore@gmail.com>
2019-03-06 20:18:42 -0800

    Remove dead code

--- Relabel Button.sketchplugin/Contents/Sketch/Relabel Button.js
+++ Relabel Button.sketchplugin/Contents/Sketch/Relabel Button.js
@@ -167,10 +167,6 @@ var relabelButton = function(context) {
 
               // apply the overrides to the symbol instance
               sel.overrides = mutableOverrides;
-
-              // deselect and reselect so the override text gets updated in the inspector
-              // sel.setIsSelected(false);
-              // sel.setIsSelected(true);
             }
           }
         )
